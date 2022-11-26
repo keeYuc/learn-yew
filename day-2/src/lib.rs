6c72e6f178cfc7e2617f5af49bd26778387a9be5
keeYuc <52500690@qq.com>
2022-11-26 23:10:33 +0800

    add log

--- day-2/src/lib.rs
+++ day-2/src/lib.rs
@@ -1,7 +1,22 @@
+use gloo::console::log;
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct MyObject {
+    username: String,
+    foverite_language: String,
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let name = "keey";
+    let my_object = MyObject {
+        username: name.to_owned(),
+        foverite_language: "rust".to_owned(),
+    };
+    log!("%d hello %s", 1, name);
+    log!(serde_json::to_string_pretty(&my_object).unwrap());
     html!(
         <h1>{"hello world"}</h1>
     )
